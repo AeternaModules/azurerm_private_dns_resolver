b9eebbeb171b453c6f88fb71996ae631dd10b92e
Daniel Allen <dan@aeternagroup.net>
2026-07-06 23:14:31 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -2,4 +2,24 @@ output "private_dns_resolvers" {
   description = "All private_dns_resolver resources"
   value       = azurerm_private_dns_resolver.private_dns_resolvers
 }
+output "private_dns_resolvers_location" {
+  description = "List of location values across all private_dns_resolvers"
+  value       = [for k, v in azurerm_private_dns_resolver.private_dns_resolvers : v.location]
+}
+output "private_dns_resolvers_name" {
+  description = "List of name values across all private_dns_resolvers"
+  value       = [for k, v in azurerm_private_dns_resolver.private_dns_resolvers : v.name]
+}
+output "private_dns_resolvers_resource_group_name" {
+  description = "List of resource_group_name values across all private_dns_resolvers"
+  value       = [for k, v in azurerm_private_dns_resolver.private_dns_resolvers : v.resource_group_name]
+}
+output "private_dns_resolvers_tags" {
+  description = "List of tags values across all private_dns_resolvers"
+  value       = [for k, v in azurerm_private_dns_resolver.private_dns_resolvers : v.tags]
+}
+output "private_dns_resolvers_virtual_network_id" {
+  description = "List of virtual_network_id values across all private_dns_resolvers"
+  value       = [for k, v in azurerm_private_dns_resolver.private_dns_resolvers : v.virtual_network_id]
+}
 
