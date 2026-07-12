00e775708f0939bc9bc9a2518de8907b2c031c80
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:19:46 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,25 +1,21 @@
-output "private_dns_resolvers" {
-  description = "All private_dns_resolver resources"
-  value       = azurerm_private_dns_resolver.private_dns_resolvers
-}
 output "private_dns_resolvers_location" {
-  description = "List of location values across all private_dns_resolvers"
-  value       = [for k, v in azurerm_private_dns_resolver.private_dns_resolvers : v.location]
+  description = "Map of location values across all private_dns_resolvers, keyed the same as var.private_dns_resolvers"
+  value       = { for k, v in azurerm_private_dns_resolver.private_dns_resolvers : k => v.location }
 }
 output "private_dns_resolvers_name" {
-  description = "List of name values across all private_dns_resolvers"
-  value       = [for k, v in azurerm_private_dns_resolver.private_dns_resolvers : v.name]
+  description = "Map of name values across all private_dns_resolvers, keyed the same as var.private_dns_resolvers"
+  value       = { for k, v in azurerm_private_dns_resolver.private_dns_resolvers : k => v.name }
 }
 output "private_dns_resolvers_resource_group_name" {
-  description = "List of resource_group_name values across all private_dns_resolvers"
-  value       = [for k, v in azurerm_private_dns_resolver.private_dns_resolvers : v.resource_group_name]
+  description = "Map of resource_group_name values across all private_dns_resolvers, keyed the same as var.private_dns_resolvers"
+  value       = { for k, v in azurerm_private_dns_resolver.private_dns_resolvers : k => v.resource_group_name }
 }
 output "private_dns_resolvers_tags" {
-  description = "List of tags values across all private_dns_resolvers"
-  value       = [for k, v in azurerm_private_dns_resolver.private_dns_resolvers : v.tags]
+  description = "Map of tags values across all private_dns_resolvers, keyed the same as var.private_dns_resolvers"
+  value       = { for k, v in azurerm_private_dns_resolver.private_dns_resolvers : k => v.tags }
 }
 output "private_dns_resolvers_virtual_network_id" {
-  description = "List of virtual_network_id values across all private_dns_resolvers"
-  value       = [for k, v in azurerm_private_dns_resolver.private_dns_resolvers : v.virtual_network_id]
+  description = "Map of virtual_network_id values across all private_dns_resolvers, keyed the same as var.private_dns_resolvers"
+  value       = { for k, v in azurerm_private_dns_resolver.private_dns_resolvers : k => v.virtual_network_id }
 }
 
